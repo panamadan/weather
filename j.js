4e082630aff7325c71918b6a15c1c4c2fddeb881
Juan De La Guardia <delaguardia@outlook.com>
2019-12-12 21:51:28 -0500

    forecast

--- j.js
+++ j.js
@@ -9,35 +9,44 @@ $(document).ready(function () {
         $.ajax({
             url: weatherUrl + searchValue + weatherApikey + "&units=imperial",
             type: "GET",
+        
+        }) .then(function (response) {
+                console.log(response)
+                $(".today").html("<h1>" + response.name + " Weather Details</h1>" + "Temperature " + response.main.temp)
+                $(".today").html("<h1>" + response.name + " Weather Details</h1>" + "Humidity " + response.main.humidity)
+                $(".today").html("<h1>" + response.name + " Weather Details</h1>" + "WindSpeed " + response.wind.speed)
 
-        }).then(function (response) {
-            console.log(response)
-            $(".today").html("<h1>" + response.name + " Weather Details</h1>" + "Temperature " + response.main.temp)
-            $(".today").html("<h1>" + response.name + " Weather Details</h1>" + "Humidity " + response.main.humidity)
-            $(".today").html("<h1>" + response.name + " Weather Details</h1>" + "WindSpeed " + response.wind.speed)
-            // add others here
-            var item = $("<li>").html($("<button>").text(searchValue))
-            $(".list").append(item)
-        })
-
-
-    }
-    function getForecast (searchValue) {
-        $.ajax({
-            url: "http://pro.openweathermap.org/data/2.5/forecast/hourly?q="
-            type: "GET",
-
-        }).then(function (response) {
-            console.log(Url)
-            console.log(response)
-            // add list to cities --appen
-
-            $("#search-button").on("click", function () {
-                var searchValue = $("#search-value").val()
-                // search weather function here
-                currentWeather(searchValue);
-
+                // add others here
+               var item = $("<li>").html($("<button>").text(searchValue))
+               $(".list").append(item)
             })
 
 
-        })
+    }
+    function getForecast(searchValue) {
+        $.ajax({
+            url: "http://pro.openweathermap.org/data/2.5/forecast/hourly?q=",
+            type: "GET",
+        }).then(function (response) {
+            console.log(Url);
+            console.log(response);
+            // add list to cities --appen
+            $("#search-button").on("click", function () {
+                var searchValue = $("#search-value").val();
+                // search weather function here
+                currentWeather(searchValue);
+            });
+        });
+    }
+    
+
+
+    $("#search-button").on("click", function () {
+        var searchValue = $("#search-value").val()
+        // search weather function here
+        currentWeather(searchValue);
+
+    })
+
+
+})
