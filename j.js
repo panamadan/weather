b2feff5e02815e9ee589ad551da6626f9ad591d3
Juan De La Guardia <delaguardia@outlook.com>
2019-12-12 21:30:52 -0500

    search box, humidity, temp, wind

---
+++ j.js
@@ -0,0 +1,43 @@
+$(document).ready(function () {
+
+    const weatherUrl = "http://api.openweathermap.org/data/2.5/weather?q="
+
+    const weatherApikey = "&apikey=643d14062ad0bb267798f46621468124"
+
+
+    function currentWeather(searchValue) {
+        $.ajax({
+            url: weatherUrl + searchValue + weatherApikey + "&units=imperial",
+            type: "GET",
+
+        }).then(function (response) {
+            console.log(response)
+            $(".today").html("<h1>" + response.name + " Weather Details</h1>" + "Temperature " + response.main.temp)
+            $(".today").html("<h1>" + response.name + " Weather Details</h1>" + "Humidity " + response.main.humidity)
+            $(".today").html("<h1>" + response.name + " Weather Details</h1>" + "WindSpeed " + response.wind.speed)
+            // add others here
+            var item = $("<li>").html($("<button>").text(searchValue))
+            $(".list").append(item)
+        })
+
+
+    }
+    function getForecast (searchValue) {
+        $.ajax({
+            url: "http://pro.openweathermap.org/data/2.5/forecast/hourly?q="
+            type: "GET",
+
+        }).then(function (response) {
+            console.log(Url)
+            console.log(response)
+            // add list to cities --appen
+
+            $("#search-button").on("click", function () {
+                var searchValue = $("#search-value").val()
+                // search weather function here
+                currentWeather(searchValue);
+
+            })
+
+
+        })
---
+++ psuedocode.md
@@ -0,0 +1,30 @@
+when user goes to the site
+    header
+    search bar
+    main panel that will dynamically display
+        city name   
+        humidity
+        wind speed
+        uv index()
+        will wrtie a conditionsl that color indicate the label
+    5 day forcast widget display
+        date
+        weather    
+        temp
+        himudity
+    if the user has already searched a city
+        it will be saved for them to click on again 
+
+notes
+
+get api key from open weather
+create on click event for the search button
+create a function to search for the weatehr
+    make an ajax call to the current weather api
+    append data to the page
+creat a function to get the forecast 
+    make an ajax call to the forecast api
+    append to the page
+create a function for the uv index
+    make ajax call to the uv api
+    append that to the snearch weather functio
